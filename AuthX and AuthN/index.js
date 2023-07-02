c3d0351b87034995766c511271ff25f41925995e
Gungun Gupta <104621253+GungunGupta14@users.noreply.github.com>
2023-07-02 23:21:10 +0530

    .;

--- AuthX and AuthN/index.js
+++ AuthX and AuthN/index.js
@@ -1,17 +1,21 @@
-const express=require("express");
-const app= express();
+const express = require("express");
+const app = express();
 
-require ('dotenv').config();
-const PORT= process.env.PORT||4000;
+require('dotenv').config();
+const PORT = process.env.PORT || 4000;
+
+//cookie-parser - what is this and why we need this ?
+
 app.use(express.json());
 
 require("./config/database").connect();
 
 //route import and mount
-const user=require("./routes/user")
-app.use("/api/v1",user);
+const user = require("./routes/user");
+app.use("/api/v1", user);
 
-//activate
-app.listen(PORT,()=>{
-    console.log(`App is running at port ${PORT}`);
+//actuivate
+
+app.listen(PORT, () => {
+    console.log(`App is listening at ${PORT}`);
 })

99ebdcba15b7c12ec6ebe167eed3dabb03a5564a
Gungun Gupta <104621253+GungunGupta14@users.noreply.github.com>
2023-07-09 18:02:42 +0530

    ..

---
+++ Mega Backend/megaBackend Class 2/models/Tags.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose");
+
+const tagsSchema=new mongoose.Schema({
+  name:{
+    type:String,
+    required:true,
+  },
+  description:{
+    type:String,
+  },
+  course:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"Courses",
+  },
+});
+module.exports= mongoose.model("Tags", yagsSchema);
